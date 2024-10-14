69b41614f7c3d7f248375564981440b8eb9ce593
Пользователь <@N00BOTIK>
2024-10-14 15:34:48 +0500

    Добавьте файлы проекта.

---
+++ kankylator/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace kankylator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.resultTextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.operand1TextBox = new System.Windows.Forms.TextBox();
+            this.operand2TextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // resultTextBox
+            // 
+            this.resultTextBox.Location = new System.Drawing.Point(268, 444);
+            this.resultTextBox.Name = "resultTextBox";
+            this.resultTextBox.Size = new System.Drawing.Size(445, 22);
+            this.resultTextBox.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(394, 216);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "+";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(394, 280);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "-";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(500, 216);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "*";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(500, 280);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "/";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // operand1TextBox
+            // 
+            this.operand1TextBox.Location = new System.Drawing.Point(156, 81);
+            this.operand1TextBox.Name = "operand1TextBox";
+            this.operand1TextBox.Size = new System.Drawing.Size(213, 22);
+            this.operand1TextBox.TabIndex = 6;
+            this.operand1TextBox.TextChanged += new System.EventHandler(this.a_TextChanged);
+            // 
+            // operand2TextBox
+            // 
+            this.operand2TextBox.Location = new System.Drawing.Point(613, 81);
+            this.operand2TextBox.Name = "operand2TextBox";
+            this.operand2TextBox.Size = new System.Drawing.Size(213, 22);
+            this.operand2TextBox.TabIndex = 7;
+            this.operand2TextBox.TextChanged += new System.EventHandler(this.b_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(987, 576);
+            this.Controls.Add(this.operand2TextBox);
+            this.Controls.Add(this.operand1TextBox);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.resultTextBox);
+            this.Name = "Form1";
+            this.Text = "Калькулятор";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox resultTextBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox operand1TextBox;
+        private System.Windows.Forms.TextBox operand2TextBox;
+    }
+}
+
---
+++ kankylator/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kankylator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void a_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void b_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            {
+                int operand1 = int.Parse(operand1TextBox.Text);
+                int operand2 = int.Parse(operand2TextBox.Text);
+                int result = operand1 + operand2;
+                resultTextBox.Text = result.ToString();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            {
+                int operand1 = int.Parse(operand1TextBox.Text);
+                int operand2 = int.Parse(operand2TextBox.Text);
+                int result = operand1 - operand2;
+                resultTextBox.Text = result.ToString();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            {
+                int operand1 = int.Parse(operand1TextBox.Text);
+                int operand2 = int.Parse(operand2TextBox.Text);
+                int result = operand1 * operand2;
+                resultTextBox.Text = result.ToString();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            {
+                int operand1 = int.Parse(operand1TextBox.Text);
+                int operand2 = int.Parse(operand2TextBox.Text);
+                int result = operand1 / operand2;
+                resultTextBox.Text = result.ToString();
+            }
+        }
+    }
+}
